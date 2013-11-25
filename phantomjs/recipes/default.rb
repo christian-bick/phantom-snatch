cff17743b8e3de54290007348dcd35fdb929f42d
christian-bick <christian.bick@craftforge.net>
2013-11-25 14:37:42 +0100

    Added phantomjs user and copying of snapshot-script to user home

--- phantomjs/recipes/default.rb
+++ phantomjs/recipes/default.rb
@@ -3,3 +3,26 @@ include_recipe "nodejs"
 execute "install phantomjs" do
   command "npm -g install phantomjs"
 end
+
+user "phantomjs" do
+  comment "User for running Phantom JS"
+  home "/home/phantomjs"
+  shell "/bin/bash"
+  system true
+  action :create
+end
+
+group "phantomjs" do
+  append true
+  members "phantomjs"
+  system true
+  action :create
+end
+
+file "snapshot-script.js" do
+  owner "phantomjs"
+  group "phantomjs"
+  path "/home/phantomjs"
+  mode "0755"
+  action :create
+end
